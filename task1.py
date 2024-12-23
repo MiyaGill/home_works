b5df29c9a09911aa9be323312714bc9ec4144d25
MiyaGill <m_gil@list.ru>
2024-12-23 15:13:48 +0300

    GalacticAdventures
     Практическое задание по уроку "Базовые структуры данных"

---
+++ task1.py
@@ -0,0 +1,15 @@
+"1st program"
+print(9**0.5*5)
+
+"2nd program"
+print(9.99>9.98, 1000!=1000.1)
+
+"3rd program"
+print(2*2+2==(2*2)+2)
+
+"4th program"
+float("123.456")
+x = 123.456
+float(123.45/10)
+print (int(1234 % 10))
+
